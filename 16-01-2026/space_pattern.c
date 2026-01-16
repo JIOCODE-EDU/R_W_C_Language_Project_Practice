a2143ebb5ee8063821b16c9209ff5b7f3015f46e
jiocode-edu <ifmdac80@gmail.com>
2026-01-16 17:47:48 +0530

    16-01-2026

---
+++ 16-01-2026/space_pattern.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+  int n = 5;
+
+  for(int i = n; i >= 1; i--){
+    // space
+    for(int k = 1; k <= n - i; k++){
+      printf(" ");
+    }
+    for(int j = 1; j <= i; j++){
+      printf("* ");
+    }
+
+    printf("\n");
+  }
+}
