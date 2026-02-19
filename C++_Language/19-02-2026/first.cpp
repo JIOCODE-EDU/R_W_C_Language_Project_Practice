81551c4bda701c830305579d7b01f83a5d14976e
jiocode-edu <ifmdac80@gmail.com>
2026-02-19 18:23:25 +0530

    19-02-2026

---
+++ C++_Language/19-02-2026/first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int number = 10;
+  
+  cout << "Vivek Kachhadiya " << number << endl;
+  cout << "Vivek Kachhadiya " << number ;
+
+  return 0;
+}
