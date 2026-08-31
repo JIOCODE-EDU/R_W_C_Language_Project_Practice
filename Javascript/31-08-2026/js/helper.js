b84003c25e7c894020a58cf98d379059d3d35a6f
jiocode-edu <ifmdac80@gmail.com>
2026-08-31 15:57:57 +0530

    31-08-2026

---
+++ Javascript/31-08-2026/js/helper.js
@@ -0,0 +1,5 @@
+import add from "./home.js"
+import { multi } from "./home.js"
+
+console.log(add(10 , 20))
+console.log(multi(5 , 10));
---
+++ Javascript/31-08-2026/js/home.js
@@ -0,0 +1,15 @@
+const render = () => {
+  document.getElementById("heading").innerHTML = "ShopEasy Ecommerce Plateform"
+}
+
+
+const add = (a , b) => {
+  return a + b
+}
+
+export const multi = (a , b) => {
+  return a * b
+}
+
+
+export default add
